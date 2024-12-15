42c8c876d16ea2bb343c8a9d6e2dc3753d830d49
Tat Hoang Nguyen <tat-nguy@c2r8p7.42perpignan.fr>
2024-12-15 18:07:24 +0100

    push

--- push_swap/operations.c
+++ push_swap/operations.c
@@ -1,14 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/14 20:40:46 by tat-nguy          #+#    #+#             */
-/*   Updated: 2024/12/15 17:03:14 by tat-nguy         ###   ########.fr       */
+/*   Created: 2024/12/15 16:10:16 by tat-nguy          #+#    #+#             */
+/*   Updated: 2024/12/15 18:04:37 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+
+//swap: sa, sb
+void	ft_swap(t_stack **head)
+{
+	t_stack	*temp;
+	
+	if (!(*head) || !(*head)->next)
+		return (NULL);
+
+	temp = *head;
+	*head = ft_lstlast(*head);
+	(*head)->next = temp;
+	*head = temp->next;
+	temp->next = NULL;
+}
+
+void	ft_push(t_stack	*dest, t_stack *src)
+{
+	int	temp;
+	
+	
+}
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,14 +6,22 @@
 /*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 20:39:57 by tat-nguy          #+#    #+#             */
-/*   Updated: 2024/12/15 10:55:02 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2024/12/15 16:56:05 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <unistd.h>
 
+typedef	struct s_stack
+{
+	int	nbr;
+	int	index;
+	struct	s_stack	*next;
+	struct	s_stack	*prev;
+}	t_stack;
 
 
 #endif
